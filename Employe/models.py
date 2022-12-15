e659e9e4c17572cf362b319cb6c8a57c19bf9197
petros manyeka <bsc-com-36-19@unima.ac.mw>
2022-12-15 01:21:59 -0800

    created models class

---
+++ Employe/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Teachers(models.Model):
+    Teac_name = models.CharField( max_length=50)
+    email = models.CharField( max_length=50)
+    password = models.CharField( max_length=50)
+    age = models.IntegerField()
+    salary = models.FloatField()
+    phone_numb = models.CharField( max_length=50)
